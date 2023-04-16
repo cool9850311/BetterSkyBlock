42b2c01f43f6fca40e1b51da8fa75335cccc3e88
施承毅 <harryshih@local>
2023-04-16 23:13:43 +0800

    feat:add change home command

---
+++ src/main/java/com/fnv_tw/commands/ChangeHome.java
@@ -0,0 +1,23 @@
+package com.fnv_tw.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ChangeHome implements TabCompleter, CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}
